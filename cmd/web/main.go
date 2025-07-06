7dc763c77ab2e2fde862b76c63e33af52bcc413f
Hardik Agarwal <141443896+hardikagarwal2026@users.noreply.github.com>
2025-07-06 15:32:59 +0530

    assing custom  helper for errors

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,18 +1,50 @@
 package main
-import(
+
+import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
-func main(){
-   mux := http.NewServeMux()
-   mux.HandleFunc("/", home)
-   mux.HandleFunc("/snippet", showSnippet)
-   mux.HandleFunc("/snippet/create", createSnippet)
-   
-   fileServer := http.FileServer(http.Dir("./ui/static/"))
-   mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-   log.Println("Starting server on : 4000")
-   err := http.ListenAndServe(":4000", mux)
-   log.Fatal(err)
+type application struct{
+	infolog *log.Logger
+	errorlog *log.Logger
+}
+
+
+func main() {
+	addr := flag.String("addr", ":4000", "HTTP Network Address")
+	flag.Parse()
+
+	infolog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorlog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Llongfile)
+
+    // Initialize a new instance of application containing the dependencies.
+	app := &application{
+		infolog: infolog,
+		errorlog: errorlog,
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet", app.showSnippet)
+	mux.HandleFunc("/snippet/create", app.createSnippet)
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+
+	srv := &http.Server{
+		Addr: *addr,
+		ErrorLog: errorlog,
+		Handler: mux,
+	}
+
+	infolog.Printf("Starting server on : %s", *addr)
+//	err := http.ListenAndServe(*addr, mux)
+    err := srv.ListenAndServe()
+
+	
+    errorlog.Fatal(err)
 }
